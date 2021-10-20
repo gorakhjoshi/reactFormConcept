88fa3008f817dbe0d367084571448e6bc7b8d9a4
Gorakh Raj Joshi <gorakh626@gmail.com>
2021-10-20 11:57:45 +0545

    extra credit 3 solution

--- src/App.js
+++ src/App.js
@@ -1,34 +1,32 @@
-// Dynamic Forms
-// 💯 Validate lower-case
+// Controlled Forms
+// 💯 Control the input value
 
 import React from 'react';
 
 function UsernameForm({ onSubmitUsername }) {
-  const [error, setError] = React.useState(null);
+  const [username, setUsername] = React.useState('');
 
   function handleSubmit(event) {
     event.preventDefault();
-    onSubmitUsername(event.target.elements.usernameInput.value);
+    onSubmitUsername(username);
   }
 
   function handleChange(event) {
-    const { value } = event.target;
-    const isLowerCase = value === value.toLowerCase();
-    setError(isLowerCase ? null : 'Username must be lower case');
+    setUsername(event.target.value.toLowerCase());
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <div>
         <label htmlFor='usernameInput'>Username:</label>
-        <input id='usernameInput' type='text' onChange={handleChange} />
+        <input
+          id='usernameInput'
+          type='text'
+          onChange={handleChange}
+          value={username}
+        />
       </div>
-      <div role='alert' style={{ color: 'red' }}>
-        {error}
-      </div>
-      <button disabled={Boolean(error)} type='submit'>
-        Submit
-      </button>
+      <button type='submit'>Submit</button>
     </form>
   );
 }
